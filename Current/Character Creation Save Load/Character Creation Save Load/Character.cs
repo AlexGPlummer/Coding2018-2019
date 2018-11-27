6ac939b065784a3b8efde8fe419912298e8f2053
AGPLum <alex.plummer.x2@gmail.com>
2018-11-27 14:42:17 -0700

    Added save character code and set up a base for modify character.

--- Current/Character Creation Save Load/Character Creation Save Load/Character.cs
+++ Current/Character Creation Save Load/Character Creation Save Load/Character.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Character_Creation_Save_Load
 {
@@ -14,9 +15,16 @@ namespace Character_Creation_Save_Load
         public int strength;
         public int speed;
         public int hunger;
-        public Character(string name, int lives, int health, int strength, int speed, int hunger)
+        public int points;
+        public Character(string name, int lives, int health, int strength, int speed, int hunger, int points)
         {
             this.name = name;
-        }
+            this.lives = lives;
+            this.health = health;
+            this.strength = strength;
+            this.speed = speed;
+            this.hunger = hunger;
+            this.points = points;
+        }        
     }    
 }
--- Current/Character Creation Save Load/Character Creation Save Load/Program.cs
+++ Current/Character Creation Save Load/Character Creation Save Load/Program.cs
@@ -9,13 +9,12 @@ namespace Character_Creation_Save_Load
 {
     class Program
     {
-        
 
+        static string name = " ";
         static void Main(string[] args)
-        {
+        {           
             int answer = 0;
-            Random rand = new Random();
-
+            Random rand = new Random();             
             while (answer != 6)
             {
                 Console.WriteLine("*************************************************************");
@@ -30,11 +29,10 @@ namespace Character_Creation_Save_Load
                 answer = int.Parse(Console.ReadLine());
                 if (answer == 1)
                 {
-                    Console.Write("What is your name: ");
-                    string name = Console.ReadLine();
-                    int st = rand.Next(10);
-                    Console.Write(st);
-                    Character susan = new Character(name, 9, 100, rand.Next(10), rand.Next(10), 50);
+                    Console.Write("What is the name of the character?: ");
+                    name = Console.ReadLine();                    
+                    Character susan = new Character(name, 9, 100, 1, 1, 50, 30);
+                    SaveCharacter(susan);
                 }
                 if (answer == 2)
                 {
@@ -57,6 +55,63 @@ namespace Character_Creation_Save_Load
                 }
             }
         }
-        
+        static void SaveCharacter(Character character)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory +  "Characters\\" + character.name + ".txt";
+            using (StreamWriter w = new StreamWriter(path))
+            {
+                w.WriteLine(character.name);
+                w.WriteLine(character.lives);
+                w.WriteLine(character.health);
+                w.WriteLine(character.strength);
+                w.WriteLine(character.speed);
+                w.WriteLine(character.hunger);
+                w.WriteLine(character.points);
+            }
+        }
+        static void ModifyCharacter(Character character)
+        {
+            int ans = 0;
+            while (ans != 4)
+            {
+                //Console.Clear();
+                Console.WriteLine("You have " + character.points + " points to spend on health, strength, or speed.");
+                Console.WriteLine("You currently have " + character.health + " health, " + character.strength + " strength, and " + character.speed + " speed.");
+                Console.Write("If you want to edit health enter 1, if you want to edit strength enter 2, if you want to edit speed press 3, if you want to exit enter 4: ");
+                ans = int.Parse(Console.ReadLine());
+                if (ans == 1)
+                {
+                    if (character.points > 0)
+                    {
+                        Console.Write("How many points do you want to increase your health by? ");
+                        int healthChange = int.Parse(Console.ReadLine());
+                        if (healthChange <= character.points)
+                        {
+
+                        }
+                        if (healthChange > character.points)
+                        {
+                            Console.WriteLine("You enter a larger number than points, health change did no go through.");
+                        }
+                    }
+                }
+
+                if (ans == 2)
+                {
+                    if (character.points > 0)
+                    {
+                        Console.Write("How many points do you want to increase your strength by? ");
+                    }
+                }
+
+                if (ans == 3)
+                {
+                    if (character.points > 0)
+                    {
+                        Console.Write("How many points do you want to increase your speed by? ");
+                    }
+                }
+            }            
+        }
     }    
 }
