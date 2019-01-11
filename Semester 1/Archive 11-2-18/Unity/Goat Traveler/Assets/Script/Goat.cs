ef1d96ac24ac52049e3b2646cef21013657239cd
AGPLum <alex.plummer.x2@gmail.com>
2019-01-11 12:13:32 -0700

    cleaning

---
+++ Semester 1/Archive 11-2-18/Unity/Goat Traveler/Assets/Script/Goat.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goat : MonoBehaviour {
+    Rigidbody2D rbody;
+
+	// Use this for initialization
+	void Start () {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+    [SerializeField]
+    float speed = 4f;
+    Vector3 velocity = new Vector3(0, 0, 0);
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (transform.position.y < 12)
+            {
+                transform.position += .5f * Vector3.up * Time.deltaTime * speed;
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (transform.position.y > -12)
+            {
+                transform.position += .5f * Vector3.down * Time.deltaTime * speed;
+            }
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (transform.position.x < 19.75f)
+            {
+                transform.position += .5f * Vector3.right * Time.deltaTime * speed;
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (transform.position.x > -19.75f)
+            {
+                transform.position += .5f * Vector3.left * Time.deltaTime * speed;
+            }
+        }
+    }       
+}
+
+
