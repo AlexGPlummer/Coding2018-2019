00350ada2d0b27a438e8f5e4abbdd4e99e91f7fa
AGPLum <alex.plummer.x2@gmail.com>
2019-02-01 13:41:56 -0700

    Generic Queue

---
+++ Semester 2/Current/Queue/Queue/GenericQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericQueue
+{
+    class QueueGeneric<T>
+    {
+        List<T> queue = new List<T>();
+
+        public QueueGeneric() { }
+
+        public void Enqueue(T val)
+        {
+            queue.Insert(0, val);
+        }
+
+        public T Dequeue()
+        {
+            T temp = queue[queue.Count - 1];
+            queue.RemoveAt(queue.Count - 1);
+            return temp;
+        }
+
+        public T Peek()
+        {
+            return queue[queue.Count - 1];
+        }
+    }
+}
--- Semester 2/Current/Queue/Queue/Program.cs
+++ Semester 2/Current/Queue/Queue/Program.cs
@@ -4,12 +4,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Queue
+namespace GenericQueue
 {
     class Program
     {
 
         static void Main(string[] args)
+        {
+            genericQueue();
+        }
+        static void genericQueue()
+        {
+            QueueGeneric<int> T = new QueueGeneric<int>();            
+            int answer = 0;
+            while (answer != 5)
+            {
+                Console.WriteLine("*************************************************************");
+                Console.WriteLine("** Enter the integer value for the action you want to perform");
+                Console.WriteLine("** 1. Add a number to the back of the Queue");
+                Console.WriteLine("** 2. Remove the front element of the Queue");
+                Console.WriteLine("** 3. See what's the first element in the Queue");
+                Console.WriteLine("** 4. Print the Queue");
+                Console.WriteLine("** 5. Exit the program");
+                Console.WriteLine("*************************************************************");
+                answer = int.Parse(Console.ReadLine());
+                if (answer == 1)
+                {
+                    //EnQueue
+                    Console.Clear();
+                    Console.WriteLine("What number would you like to add to the Queue");
+                    int y = int.Parse(Console.ReadLine());
+                    Console.WriteLine("*************************************************************");
+                    T.EnQueue(y);
+                }
+                if (answer == 2)
+                {
+                    Console.Clear();
+                    //DeQueue
+                    Console.WriteLine("*************************************************************");
+                    T.DeQueue();
+                }
+                if (answer == 3)
+                {
+                    Console.Clear();
+                    //Peek
+                    Console.WriteLine("*************************************************************");
+                    T.Peek();
+                }
+                if (answer == 4)
+                {
+                    Console.Clear();
+                    //Print
+                    Console.WriteLine("*************************************************************");
+                    //T.Print();
+                }
+            }
+        }
+
+    }
+}
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Queue();
+        }
+
+        static void Queue()
         {
             Queue myQueue = new Queue();
             int answer = 0;
