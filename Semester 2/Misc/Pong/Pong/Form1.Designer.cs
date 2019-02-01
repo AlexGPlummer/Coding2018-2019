e54d303d096405b5120fa667bb5f3d2c47e76931
AGPLum <alex.plummer.x2@gmail.com>
2019-02-01 13:15:48 -0700

    Merge branch 'master' of https://github.com/AlexGPlummer/Coding2018-2019
    # Conflicts:
    #	Semester 2/Current/Card Game/Card Game/Program.cs
    #	Semester 2/Current/Inheritance/Inheritance/Memes.cs
    #	Semester 2/Current/Inheritance/Inheritance/Program.cs
    #	Semester 2/Misc/Pong/Pong/Form1.Designer.cs

---
+++ Semester 2/Misc/Pong/Pong/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player1Score = new System.Windows.Forms.Label();
+            this.player2Score = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player1
+            // 
+            this.player1.AccessibleName = "Player1";
+            this.player1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(60)))));
+            this.player1.Location = new System.Drawing.Point(12, 186);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(27, 120);
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.AccessibleName = "Ball";
+            this.ball.BackColor = System.Drawing.Color.Black;
+            this.ball.Location = new System.Drawing.Point(434, 239);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(27, 26);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // player2
+            // 
+            this.player2.AccessibleName = "Player2";
+            this.player2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(60)))), ((int)(((byte)(30)))));
+            this.player2.Location = new System.Drawing.Point(875, 186);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(27, 120);
+            this.player2.TabIndex = 2;
+            this.player2.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.timerTick);
+            // 
+            // player1Score
+            // 
+            this.player1Score.AutoSize = true;
+            this.player1Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1Score.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(60)))));
+            this.player1Score.Location = new System.Drawing.Point(105, 9);
+            this.player1Score.Name = "player1Score";
+            this.player1Score.Size = new System.Drawing.Size(57, 39);
+            this.player1Score.TabIndex = 3;
+            this.player1Score.Text = "00";
+            // 
+            // player2Score
+            // 
+            this.player2Score.AutoSize = true;
+            this.player2Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2Score.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(60)))), ((int)(((byte)(30)))));
+            this.player2Score.Location = new System.Drawing.Point(730, 9);
+            this.player2Score.Name = "player2Score";
+            this.player2Score.Size = new System.Drawing.Size(57, 39);
+            this.player2Score.TabIndex = 4;
+            this.player2Score.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(100)))), ((int)(((byte)(170)))));
+            this.ClientSize = new System.Drawing.Size(912, 525);
+            this.Controls.Add(this.player2Score);
+            this.Controls.Add(this.player1Score);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.player1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox player2;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label player1Score;
+        private System.Windows.Forms.Label player2Score;
+    }
+}
+
---
+++ Semester 2/Misc/Pong/Pong/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class Form1 : Form
+    {
+        bool goup;
+        bool godown;
+        bool goup2;
+        bool godown2;
+        int speed = 5;
+        int ballx = 5;
+        int bally = 5;
+        int player1score = 0;
+        int player2score = 0;
+
+        public Form1()
+        {            
+            InitializeComponent();                   
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goup2 = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godown2 = true;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                goup = true;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                godown = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goup2 = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                godown2 = false;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                goup = false;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                godown = false;
+            }
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            player1Score.Text = "" + player1score;
+            player2Score.Text = "" + player2score;
+
+            ball.Top -= bally;
+            ball.Left -= ballx;
+
+            if (ball.Left < 0)
+            {
+                ball.Left = 434;
+                ballx = -ballx;
+                ballx -= 2;
+                player2score++;
+            }
+
+            if (ball.Left + ball.Width > ClientSize.Width)
+            {
+                ball.Left = 434;
+                ballx = -ballx;
+                ballx += 2;
+                player1score++;
+            }
+
+            if (ball.Top < 0 || ball.Top + ball.Height > ClientSize.Height)
+            {
+                bally = -bally;
+            }
+
+            if (ball.Bounds.IntersectsWith(player1.Bounds) || ball.Bounds.IntersectsWith(player2.Bounds))
+            {
+                ballx = -ballx;
+            }
+
+            if (goup == true && player1.Top > 0)
+            {
+                player1.Top -= 8;
+            }
+
+            if (godown == true && player1.Top < 455)
+            {
+                player1.Top += 8;
+            }
+
+            if (goup2 == true && player2.Top > 0)
+            {
+                player2.Top -= 8;
+            }
+
+            if (godown2 == true && player2.Top < 455)
+            {
+                player2.Top -= 8;
+            }
+
+            if (player1score >= 10 && player1score - player2score != 1 && player2score - player1score != 1)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Player 1 wins!");
+            }
+
+            if (player2score >= 10 && player1score - player2score != 1 && player2score - player1score != 1)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Player 2 wins!");
+            }
+        }
+    }
+}
