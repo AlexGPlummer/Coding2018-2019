1e80c06d29d9216035b236be85a10b2641defd37
AGPLum <alex.plummer.x2@gmail.com>
2019-02-01 12:10:11 -0700

    changes

--- Semester 2/Current/Stack/Stack/Program.cs
+++ Semester 2/Current/Stack/Stack/Program.cs
@@ -10,31 +10,48 @@ namespace Stack
     {
         static void Main(string[] args)
         {
+            Stack myQueue = new Stack();
             int answer = 0;
-            Stack myStack = new Stack();
-            while (answer != 4)
+            while (answer != 5)
             {
                 Console.WriteLine("*************************************************************");
                 Console.WriteLine("** Enter the integer value for the action you want to perform");
-                Console.WriteLine("** 1. ");
-                Console.WriteLine("** 2. ");
-                Console.WriteLine("** 3. ");
-                Console.WriteLine("** 4. Exit the program");
+                Console.WriteLine("** 1. Add a number to the back of the Queue");
+                Console.WriteLine("** 2. Remove the front element of the Queue");
+                Console.WriteLine("** 3. See what's the first element in the Queue");
+                Console.WriteLine("** 4. Print the Queue");
+                Console.WriteLine("** 5. Exit the program");
                 Console.WriteLine("*************************************************************");
                 answer = int.Parse(Console.ReadLine());
                 if (answer == 1)
                 {
-
+                    //EnQueue
+                    Console.Clear();
+                    Console.WriteLine("What number would you like to add to the Queue");
+                    int y = int.Parse(Console.ReadLine());
+                    Console.WriteLine("*************************************************************");
+                    myQueue.EnQueue(y);
                 }
-
                 if (answer == 2)
                 {
-
+                    Console.Clear();
+                    //DeQueue
+                    Console.WriteLine("*************************************************************");
+                    myQueue.DeQueue();
                 }
-
                 if (answer == 3)
                 {
-
+                    Console.Clear();
+                    //Peek
+                    Console.WriteLine("*************************************************************");
+                    myQueue.Peek();
+                }
+                if (answer == 4)
+                {
+                    Console.Clear();
+                    //Print
+                    Console.WriteLine("*************************************************************");
+                    myQueue.Print();
                 }
             }
         }
--- Semester 2/Current/Stack/Stack/Stack.cs
+++ Semester 2/Current/Stack/Stack/Stack.cs
@@ -8,19 +8,55 @@ namespace Stack
 {
     class Stack
     {
-        int o;
-        List<int> hold = new List<int>();
-        public int Pop;
-        public int Peek;
-
-        public void Push(int val)
+        int myInt;
+        List<int> Q = new List<int>();
+        public int DeQueue()
         {
-
-        }  
-        
+            if (Q.Count < 1)
+            {
+                Console.WriteLine("There are no integers in the list");
+            }
+            else
+            {
+                myInt = Q[0];
+                Q.Remove(Q[0]);
+                Console.WriteLine("The first element has been removed.");
+            }
+            return myInt;
+        }
+        public void EnQueue(int val)
+        {
+            Q.Add(val);
+            Console.WriteLine("The interger " + val + " has been added to the Queue");
+        }
+        public int Peek()
+        {
+            if (Q.Count > 0)
+            {
+                myInt = Q[0];
+                Console.WriteLine("The first element is " + myInt);
+            }
+            else
+            {
+                Console.WriteLine("There are no integers in the list");
+            }
+            return myInt;
+        }
         public void Print()
         {
-
+            if (Q.Count > 0)
+            {
+                Console.Write("| ");
+                foreach (int i in Q)
+                {
+                    Console.Write(i + " | ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("There are no intergers in the Queue");
+            }
         }
     }
 }
